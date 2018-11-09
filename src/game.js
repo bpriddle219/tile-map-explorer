808d7a055bb25004fc88b709d7dc72568e4425f2
Briana Priddle <bpriddle@ksu.edu>
2018-11-09 12:00:48 -0600

    Completed initial project

--- src/game.js
+++ src/game.js
@@ -1,4 +1,5 @@
 import Input from './input';
+import Map from './tilemap.json';
 
 /** @class Game
   * A class representing the high-level functionality
@@ -17,6 +18,14 @@ export default class Game {
     this.input = new Input();
     this.entities = [];
 
+
+    this.tile_data = Map.layers[0].data;
+    this.tiles_width = Map.width;
+    this.tiles_height = Map.height;
+    this.tile_size = Map.tileheight;
+    this.tileset = new Image();
+    this.tileset.src = "tileset.png";
+
     // Set up the back buffer
     this.backBuffer = document.createElement('canvas');
     this.backBuffer.width = this.WIDTH;
@@ -30,6 +39,7 @@ export default class Game {
     this.screenBufferCtx = this.screenBuffer.getContext('2d');
     document.body.append(this.screenBuffer);
   }
+
   /** @method addEntity
     * Adds an entity to the game world
     * Entities should have an update() and render()
@@ -39,6 +49,7 @@ export default class Game {
   addEntity(entity) {
     this.entities.push(entity);
   }
+
   /** @method update
     * Updates the game state
     * @param {integer} elapsedTime - the number of milliseconds per frame
@@ -51,6 +62,7 @@ export default class Game {
     // Swap input buffers
     this.input.update();
   }
+
   /** @method render
     * Renders the game state
     * @param {integer} elapsedTime - the number of milliseconds per frame
@@ -60,7 +72,17 @@ export default class Game {
     this.backBufferCtx.fillStyle = "white";
     this.backBufferCtx.fillRect(0,0,this.WIDTH, this.HEIGHT);
 
-    // TODO: Render game
+    var count = 0;
+    for (var i = 0; i < this.tiles_height*this.tile_size; i+= this.tile_size) {
+      for (var j = 0; j < this.tiles_width*this.tile_size; j+= this.tile_size) {
+        var index = this.tile_data[count];
+        var rowIndex = index > 8 ?  this.tile_size : 0;
+        var colIndex = ((index % 8) - 1) * this.tile_size;
+        this.backBufferCtx.drawImage(this.tileset, colIndex, rowIndex, this.tile_size, this.tile_size,
+                                      j, i, this.tile_size, this.tile_size);
+        count++;
+      }
+    }
 
     // Render entities
     this.entities.forEach(entity => entity.render(elapsedTime, this.backBufferCtx));
@@ -68,6 +90,7 @@ export default class Game {
     // Flip the back buffer
     this.screenBufferCtx.drawImage(this.backBuffer, 0, 0);
   }
+
   /** @method loop
     * Updates and renders the game,
     * and calls itself on the next draw cycle.
--- src/player.js
+++ src/player.js
@@ -1,3 +1,5 @@
+import Map from './tilemap.json';
+
 /** @module Player
   * A class representing the player.
   */
@@ -7,9 +9,23 @@ export default class Player {
     * @param {float} x - the player's x position
     * @param {float} y - the player's y position
     */
-  constructor(x, y) {
+  constructor(x, y, dir, moving) {
     this.x = x;
     this.y = y;
+    this.dir = dir;
+    this.moving = moving;
+    this.tileset = new Image();
+    this.tileset.src = "tileset.png";
+    this.frameCount = 0;
+    this.switch = 0;
+
+    var a = [];
+    Map.layers[1].objects.forEach(function(obj) {
+      var thing = { x: obj.x, y: obj.y-32 };
+      a.push(thing);
+    });
+
+    this.no_go = a;
   }
 
   /** @method update
@@ -18,10 +34,82 @@ export default class Player {
     * @param {Input} input - the input object
     */
   update(deltaT, input) {
-    if(input.keyPressed("ArrowLeft")) this.x--;
-    if(input.keyPressed("ArrowRight")) this.x++;
-    if(input.keyPressed("ArrowUp")) this.y--;
-    if(input.keyPressed("ArrowDown")) this.y++;
+    if((!this.frameCount && input.keyDown("ArrowLeft")) || (this.frameCount && this.dir == 2)) {
+      this.dir = 2;
+      if (!this.frameCount && !this.checkForCollisions(Math.round(this.x-32), Math.round(this.y))) {
+       return;
+     }
+     else {
+       this.x -= 1.6;
+       this.moving = true;
+       this.frameCount++;
+       if (this.frameCount == 20) {
+         this.frameCount = 0;
+         this.x = Math.round(this.x);
+       }
+     }
+    }
+    else if((!this.frameCount && input.keyDown("ArrowRight")) || (this.frameCount && this.dir == 3)) {
+      this.dir = 3;
+      if (!this.frameCount && !this.checkForCollisions(Math.round(this.x+32), Math.round(this.y))) {
+        return
+      }
+      else {
+       this.x += 1.6;
+       this.moving = true;
+       this.frameCount++;
+       if (this.frameCount == 20) {
+         this.frameCount = 0;
+         this.x = Math.round(this.x);
+       }
+     }
+    }
+    else if((!this.frameCount && input.keyDown("ArrowUp")) || (this.frameCount && this.dir == 1)) {
+      this.dir = 1;
+      if (!this.frameCount && !this.checkForCollisions(Math.round(this.x), Math.round(this.y-32))) {
+        return;
+      }
+      else {
+        this.y -= 1.6;
+        this.moving = true;
+        this.frameCount++;
+        if (this.frameCount == 20) {
+          this.frameCount = 0;
+          this.y = Math.round(this.y);
+        }
+      }
+    }
+    else if((!this.frameCount && input.keyDown("ArrowDown")) || (this.frameCount && this.dir == 0)) {
+      this.dir = 0;
+      if (!this.frameCount && !this.checkForCollisions(Math.round(this.x), Math.round(this.y+32))) {
+        return;
+      }
+      else {
+       this.y += 1.6;
+       this.moving = true;
+       this.frameCount++;
+       if (this.frameCount == 20) {
+         this.frameCount = 0;
+         this.y = Math.round(this.y);
+       }
+     }
+    }
+    else {
+      this.moving = false;
+      this.switch = 0;
+    }
+  }
+
+  checkForCollisions(x, y) {
+    x = Math.abs(x);
+    y = Math.abs(y);
+    var length = this.no_go.length;
+    for (var i = 0; i < length; i++) {
+      if (x == this.no_go[i].x && y == this.no_go[i].y) {
+        return false;
+      }
+    }
+    return true;
   }
 
   /** @method render
@@ -30,10 +118,77 @@ export default class Player {
     * @param {Context2D} context - the rendering context
     */
   render(deltaT, context) {
-    context.fillStyle = "blue";
     context.beginPath();
-    context.arc(this.x, this.y, 25, 0, 2*Math.PI);
-    context.fill();
+    switch (this.dir) {
+      case 0:
+        if (this.moving) {
+          if (Math.round(this.frameCount) % 6 == 0) {
+            this.switch = (this.switch + 1) % 2;
+          }
+
+          if (this.switch) {
+            context.drawImage(this.tileset, 32, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+          else {
+            context.drawImage(this.tileset, 0, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+        }
+        else {
+          context.drawImage(this.tileset, 0, 0, 32, 32, this.x, this.y, 32, 32);
+        }
+        break;
+      case 1:
+        if (this.moving) {
+          if (Math.round(this.frameCount) % 6 == 0) {
+            this.switch = (this.switch + 1) % 2;
+          }
+
+          if (this.switch) {
+            context.drawImage(this.tileset, 96, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+          else {
+            context.drawImage(this.tileset, 64, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+        }
+        else {
+          context.drawImage(this.tileset, 64, 0, 32, 32, this.x, this.y, 32, 32);
+        }
+        break;
+      case 2:
+        if (this.moving) {
+          if (Math.round(this.frameCount) % 7 == 0) {
+            this.switch = (this.switch + 1) % 2;
+          }
+
+          if (this.switch) {
+            context.drawImage(this.tileset, 160, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+          else {
+            context.drawImage(this.tileset, 128, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+        }
+        else {
+          context.drawImage(this.tileset, 128, 0, 32, 32, this.x, this.y, 32, 32);
+        }
+        break;
+      case 3:
+        if (this.moving) {
+          if (Math.round(this.frameCount) % 7 == 0) {
+            this.switch = (this.switch + 1) % 2;
+          }
+
+          if (this.switch) {
+            context.drawImage(this.tileset, 224, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+          else {
+            context.drawImage(this.tileset, 192, 0, 32, 32, this.x, this.y, 32, 32);
+          }
+        }
+        else {
+          context.drawImage(this.tileset, 192, 0, 32, 32, this.x, this.y, 32, 32);
+        }
+        break;
+    }
   }
 
 }
